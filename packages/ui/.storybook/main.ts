5276ddd37b36cf9b7b52bce4668c82de6c0ff5c7
Rico Herwig <rico@hrwg.de>
2025-02-28 17:03:30 +0100

    chore: update deps
    Signed-off-by: Rico Herwig <rico@hrwg.de>

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -15,7 +15,6 @@ const config: StorybookConfig = {
         getAbsolutePath('@storybook/addon-links'),
         getAbsolutePath('@storybook/addon-essentials'),
         getAbsolutePath('@storybook/addon-interactions'),
-        getAbsolutePath('storybook-addon-theme-changer'),
     ],
     framework: {
         name: getAbsolutePath('@storybook/react-vite'),
