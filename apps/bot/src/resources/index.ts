3966f87d162501b0ec7ed995130fd19761697ce6
Rico Herwig <rico@hrwg.de>
2025-03-01 21:42:38 +0100

    feat(bot): add remaining characters to movelist bot

--- apps/bot/src/resources/index.ts
+++ apps/bot/src/resources/index.ts
@@ -1,8 +1,16 @@
-import kazuya from './moves/kazuya.json'
+import { readdirSync, readFileSync } from 'node:fs'
+import { join, parse } from 'node:path'
 
-export const movesList = [
-    {
-        character: 'kazuya',
-        moves: kazuya,
-    },
-]
+const resourcesPath = join(import.meta.dirname, './moves')
+
+export const movesList = readdirSync(resourcesPath)
+    .filter((file) => file.endsWith('.json'))
+    .map((file) => {
+        const character = parse(file).name
+        const moves = JSON.parse(
+            readFileSync(join(resourcesPath, file), 'utf-8'),
+        )
+        return { character, moves }
+    })
+
+console.log(movesList)
